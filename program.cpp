13108752a425fa5434f0301962895503f990336d
Iamlucid111 <116554700+Iamlucid111@users.noreply.github.com>
2022-12-22 11:38:47 +0100

    1

---
+++ program.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <conio.h>
+ void main (){
+
+  gotoxy(30,2);printf("gestion de voiture:");
+  gotoxy(10,4);printf("Numero:");
+  gotoxy(10,6);printf("Nom:");
+  gotoxy(10,8);printf("Nationalité:");
+  gotoxy(10,10);printf("sit-familiale:");
+  gotoxy(10,12);printf("Num de place:");
+  gotoxy(10,14);printf("prix plien tarif:");
+
+
+
+
+
+
+
+
+
+
+ getch();
+ }
